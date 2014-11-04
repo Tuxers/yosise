b059e3481d41e440ce2b541391af97b14c01424f
jhtan <jhtan@jhtan.com>
2014-11-04 05:26:28 -0400

    Create question popup finished.

---
+++ templates/pages/script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by jhtan on 11/4/14.
+ */
+(function ($) {
+    $(document).ready( function () {
+        $('#askQuestion').click( function () {
+            $('#askQuestionPopup').modal('show');
+        });
+    });
+} )(jQuery);
